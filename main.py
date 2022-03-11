98e0242ca3e63eea86701f3e01f3ae52fe2c788a
Colepng <gamingwithcole7@gmail.com>
2022-03-11 17:12:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import json
 
 f = open("C:\\Users\gamin\\OneDrive\\Documents\\GitHub\\Frcdiscordbot\\config.json",)
 
-get_token = json.load(f)
-TOKEN = get_token["token"]
-TEST = get_token["test"]
+config = json.load(f)
+TOKEN = config["token"]
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    print(TEST)
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client.user:
+        return
 
 client.run(TOKEN)
