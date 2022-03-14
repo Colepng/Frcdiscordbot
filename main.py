7ba04722a739eccc11722d8a292ac7a00f201ac3
Colepng <gamingwithcole7@gmail.com>
2022-03-14 15:10:18 -0400

    Hope heroku

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-import discord
-import json
+#Here I was importing the json libary to hide keys and importing discord.ext commands moudle and discord.py itself
+import discord          
 
+from dotenv import load_dotenv
+from os import getenv 
 from discord.ext import commands
 
+#Here I am allowing my bot to see the members in a server and there voice status
 intents = discord.Intents.default()
 intents.members = True
 intents.voice_states = True
 
+#Here I am setting the command prefix to !
+
 bot = commands.Bot(command_prefix="!", intents = intents)
+
+#Here I'm loading the python files where I stored my commands
 
 bot.load_extension("cogs.somecommands")
 bot.load_extension("cogs.Tesging button")
 bot.load_extension("cogs.newcommands")
 bot.load_extension("cogs.FrcAPI")
 
-f = open("C:\\Users\\gamin\\OneDrive\\Documents\\Code\\Frcbot\\config.json",)
-config_file = json.load(f)
-TOKEN = config_file["token"]
+
+#Here I am opening a json file that has my discord bot token and getting that token and puting it into a constent
+
+
+load_dotenv()
+TOKEN = getenv("TOKEN")
 bot.run(TOKEN)
