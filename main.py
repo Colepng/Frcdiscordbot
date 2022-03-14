e3431b31d189d099838344b3cf71919c7a077db4
Colepng <gamingwithcole7@gmail.com>
2022-03-14 17:04:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Here I was importing the json libary to hide keys and importing discord.ext commands moudle and discord.py itself
 import discord          
 
-import os
+from dotenv import load_dotenv
+from os import getenv 
 from discord.ext import commands
 
 #Here I am allowing my bot to see the members in a server and there voice status
@@ -23,6 +24,8 @@ bot.load_extension("cogs.FrcAPI")
 
 #Here I am opening a json file that has my discord bot token and getting that token and puting it into a constent
 
-TOKEN = os.getenv("token")
+
+load_dotenv()
+TOKEN = getenv("TOKEN")
 print(str(TOKEN) * 20)
 bot.run(TOKEN)
